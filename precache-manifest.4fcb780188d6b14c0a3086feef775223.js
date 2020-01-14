cd9c5afb391cf2031832140adca12dc4080f4bb8
Alex Pham <925workk@gmail.com>
2020-01-13 18:26:25 -0700

    Updates

---
+++ precache-manifest.4fcb780188d6b14c0a3086feef775223.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57f795a265a403ee15e306ef172db940",
+    "url": "/react-memory-game/index.html"
+  },
+  {
+    "revision": "f8598aec481b92916751",
+    "url": "/react-memory-game/static/css/main.3f6c4d4d.chunk.css"
+  },
+  {
+    "revision": "7809a5cd5af7ca869a3b",
+    "url": "/react-memory-game/static/js/2.a979c184.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-memory-game/static/js/2.a979c184.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f8598aec481b92916751",
+    "url": "/react-memory-game/static/js/main.70fce2a4.chunk.js"
+  },
+  {
+    "revision": "26f90137cacf9167286b",
+    "url": "/react-memory-game/static/js/runtime-main.c427298a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-memory-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
